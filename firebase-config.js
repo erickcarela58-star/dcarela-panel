9b7ea357a2e5eaf4273900251e454072397e352f
Erikc <erikc@MacBook-Air-de-Erikc.local>
2026-08-19 07:39:14 -0400

    feat(firebase): integrate Firebase project erikccarela and hosting config

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.__DCARELA_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDqcLYgNqjgkib666vQDQjP5SmDbXAcUVE",
+  authDomain: "erikccarela.firebaseapp.com",
+  projectId: "erikccarela",
+  storageBucket: "erikccarela.firebasestorage.app",
+  messagingSenderId: "1025242292135",
+  appId: "1:1025242292135:web:22faf94cf230f9ab05e082",
+  measurementId: "G-H16J1ZZH7L"
+};
